1ba9bd95102d2f6096838c5a4f2cb050fa42e77a
Victor <cx.zhu@mstglobal.com>
2019-03-05 21:11:41 +0800

    add new feature: support wrap and enter

--- external/detectkey/detectkey.c
+++ external/detectkey/detectkey.c
@@ -14,6 +14,18 @@
 #define RIGHT_DOWN 681
 #define LEFT_UP  25
 #define LEFT_DOWN 510
+#define DISPLAY_NUM 6
+
+unsigned char g_to_display[DISPLAY_NUM][32] = {
+"1111111111111111",
+"22222222222222222222222222",
+"3333333333333333",
+"4444444444444444",
+"5555555555555555",
+"6666666666666666",
+};
+
+int g_curNum = 0;
 
 void milliseconds_sleep(unsigned long mSec){
     struct timeval tv;
@@ -36,6 +48,32 @@ void opscreen(char cBuf[]) {
     close(fd);
 }
 
+void displayArray() {
+    int i = 0;
+	unsigned char displayStr[200] = {0};
+	unsigned char posStr[40] = {0};
+	int tmpNum = 0;
+	int offset = 0;
+	for (i = 0; i < 4; i++) {
+		tmpNum = g_curNum + i;
+		if (tmpNum >= DISPLAY_NUM) {
+			tmpNum = tmpNum - DISPLAY_NUM;
+		}
+		if (offset == 0) {
+			snprintf(posStr, 40, "0200%s\r\n", g_to_display[tmpNum]);
+		} else if (offset > 4) {
+			break;
+		} else {
+			snprintf(posStr, 40, "02%2d%s\r\n", 16*offset, g_to_display[tmpNum]);
+		}
+		offset = (strlen(g_to_display[tmpNum]) -1) / 16 + offset;
+		offset++;
+		strcat(displayStr, posStr);
+	}
+	printf("the displayStr is %s", displayStr);
+	opscreen(displayStr);
+}
+
 void handleKey(int fd) {
     int readNum = 0;
     int readings = 0;
@@ -51,19 +89,27 @@ void handleKey(int fd) {
     if ((DEFAULT < readings + 25) && (DEFAULT > readings - 25)) {
     	return;
     } else if ((RIGHT_UP < readings + 25) && (RIGHT_UP > readings - 25)) {
-    	opscreen("x22y22right up\r\n");
+		g_curNum++;
+		if (g_curNum > DISPLAY_NUM) {
+			g_curNum = 0;
+		}
+		displayArray();
     	return;
     } else if ((RIGHT_DOWN < readings + 25) && (RIGHT_DOWN > readings - 25)) {
-    	opscreen("x22y22right down\r\n");
+		g_curNum--;
+		if (g_curNum < 0) {
+			g_curNum = DISPLAY_NUM;
+		}
+		displayArray();
     	return;
     } else if ((LEFT_DOWN < readings + 25) && (LEFT_DOWN > readings - 25)) {
-    	opscreen("x22y22left down\r\n");
+    	opscreen("2222left down\r\n");
     	return;
     } else if ((LEFT_UP < readings + 25) && (LEFT_UP > readings - 25)) {
-    	opscreen("x22y22left up\r\n");
+    	opscreen("2222left up\r\n");
     	return;
     } else {
-    	opscreen("x22y22to do\r\n");
+    	//opscreen("x22y22to do\r\n");
     }
 }
 
